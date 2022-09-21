efba6b9487027e58572ebf1dbcb9ab71b22bb22d
MalcolmSav <malcolmsavmarker@gmail.com>
2022-09-21 09:48:47 +0200

    Insaneheaderofdoom

--- frontend/fmeca-frontend/src/components/VisHeader.vue
+++ frontend/fmeca-frontend/src/components/VisHeader.vue
@@ -1,6 +1,15 @@
 <template>
-    <main>
-        Demo
+    <main class = "fortnite">
+        Demo - fortnite
         <br><br>
     </main>
 </template>
+<style> 
+.fortnite {
+color:blueviolet;
+background-color:aqua;
+margin-top: -1%;
+margin-left: -1%;
+margin-bottom: 0%;
+}
+</style>
