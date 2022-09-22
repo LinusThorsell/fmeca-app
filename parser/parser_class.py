f3b9a7b7104682f8de9e1ae5278ac7fce028f197
Oscar <oscar.sandell@live.se>
2022-09-22 11:03:50 +0200

    Fixat så man kan skriva på commandlinen med argume

--- parser/parser_class.py
+++ parser/parser_class.py
@@ -5,14 +5,19 @@ class Parser:
 
     def __init__(self):
         self._debug = False
+        self._delete = False
+        self._add = False
         self._arguments = []
         self._flags = {}
+        self._nr_arguments = 0
 
-    def delete(self):
-        print("Vi är i delete")
+    def delete(self,project):
+        ##Tell the database to delete the project
+        print("Vi är i delete, project = " + str(project))
 
-    def add(self):
-        print("Vi är i add")
+    def add(self,path):
+        ##Add this project given by the path to the database
+        print("Vi är i add, path = " + str(path))
 
     def print(self):
         print("Vi är i print")
@@ -28,16 +33,29 @@ class Parser:
         pass
 
     def analyse_cli(self):
-        
-        for arguments in self._arguments:
-            temp_argument = arguments.lower()
-            print(temp_argument)
-            if(temp_argument in self._flags):
-                if(self._flags[temp_argument] == 0):
+        previous_was_two_part_argument = False
+        #for i in range(self._nr_arguments):
+        i = 0
+        while i < self._nr_arguments:
+            temp_argument = self._arguments[i]
+            if (temp_argument in self._flags):
+                if self._flags[temp_argument] == 0:
                     self._functions[temp_argument]()
-                if(self._flags[temp_argument] == 1):
-                    pass
-                    #self._functions[temp_argument](arg)
+                    previous_was_two_part_argument = False
+                elif self._flags[temp_argument] == 1:
+                    if i+1 < self._nr_arguments:
+                        #print("i == " + str(i) + " nr_arguments == " + str(self._nr_arguments) )
+                        argument = self._arguments[i+1]
+                    else:
+                        print("Not enough arguments for the flag")
+                        exit()
+                    self._functions[temp_argument](argument)
+                    previous_was_two_part_argument = True
+
+            elif temp_argument not in self._flags and previous_was_two_part_argument:
+                previous_was_two_part_argument = False
+                i+=1
+                continue
             else:
                 print("The flag(s) you used is not valid!")
                 print("The valid flags are:")
@@ -45,19 +63,22 @@ class Parser:
                     print(flags)
                 print("If the flag requires a path, you put the path right after the flag\n-flag -path")
                 exit()
-    
+            i+=1
+
     ##If you want to add more flags and corresponding functions you simply
     ##add the flag and function to the dictionary below
     ##in self._flags we should have the flag and how many arguments we should
     ## have after that
     def initialize(self):
-        self._flags = {"print":0,"add":0,"delete":0}
+        self._flags = {"print":0,"add":1,"delete":1}
         self._functions = {"print":self.print,"add":self.add,"delete":self.delete}
 
     def get_arguments(self):
-        nr_arguments = len(sys.argv)
-        if(nr_arguments >= 1):
-            self._arguments = sys.argv[1:nr_arguments]
+        nrarguments = len(sys.argv)
+        if(nrarguments >= 1):
+            self._arguments = sys.argv[1:nrarguments]
+            self._nr_arguments = len(self._arguments)
+
         print(self._arguments)
 
 
