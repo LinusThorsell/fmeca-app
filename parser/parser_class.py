0fdb162c80414b467a5c3b1afbbd6d5e06949f66
oscsa604 <oscsa604@ad.liu.se>
2022-09-22 14:30:40 +0200

    Ambigous amount of arguments

--- parser/parser_class.py
+++ parser/parser_class.py
@@ -9,15 +9,16 @@ class Parser:
         self._add = False
         self._arguments = []
         self._flags = {}
+        self._functions = {}
         self._nr_arguments = 0
 
     def delete(self,project):
         ##Tell the database to delete the project
         print("Vi är i delete, project = " + str(project))
 
-    def add(self,path):
+    def add(self,xml_file_path):
         ##Add this project given by the path to the database
-        print("Vi är i add, path = " + str(path))
+        print("Vi är i add, path = " + str(xml_file_path))
 
     def print(self):
         print("Vi är i print")
@@ -29,33 +30,30 @@ class Parser:
         for child in root:
             print(child)
     
+    def test_func_with_two_args(self, arg1, arg2):
+        print("test_func_with_two_args")
+    
     def send_to_database(self):
         pass
 
+    def config_database(self,path):
+        print("Configuring database, path to file = " + str(path))
+
+
     def analyse_cli(self):
-        previous_was_two_part_argument = False
+        #previous_was_two_part_argument = False
         #for i in range(self._nr_arguments):
         i = 0
         while i < self._nr_arguments:
-            temp_argument = self._arguments[i]
+            #argument_list = []
+            temp_argument = self._arguments[i].lower()
             if (temp_argument in self._flags):
-                if self._flags[temp_argument] == 0:
-                    self._functions[temp_argument]()
-                    previous_was_two_part_argument = False
-                elif self._flags[temp_argument] == 1:
-                    if i+1 < self._nr_arguments:
-                        #print("i == " + str(i) + " nr_arguments == " + str(self._nr_arguments) )
-                        argument = self._arguments[i+1]
-                    else:
-                        print("Not enough arguments for the flag")
-                        exit()
-                    self._functions[temp_argument](argument)
-                    previous_was_two_part_argument = True
-
-            elif temp_argument not in self._flags and previous_was_two_part_argument:
-                previous_was_two_part_argument = False
-                i+=1
-                continue
+                nr_arguments = self._flags[temp_argument]
+                print("Nr argumets = " + str(nr_arguments))
+                argument_list = self._arguments[i+1:i+1+nr_arguments]
+                print(argument_list)
+                self._functions[temp_argument](*argument_list)
+                i+=nr_arguments + 1
             else:
                 print("The flag(s) you used is not valid!")
                 print("The valid flags are:")
@@ -63,50 +61,20 @@ class Parser:
                     print(flags)
                 print("If the flag requires a path, you put the path right after the flag\n-flag -path")
                 exit()
-            i+=1
 
     ##If you want to add more flags and corresponding functions you simply
     ##add the flag and function to the dictionary below
     ##in self._flags we should have the flag and how many arguments we should
     ## have after that
     def initialize(self):
-        self._flags = {"print":0,"add":1,"delete":1}
-        self._functions = {"print":self.print,"add":self.add,"delete":self.delete}
+        self._flags = {"print":0,"add":1,"delete":1,"-c":1, "test":2}
+        self._functions = {"print":self.print,"add":self.add,"delete":self.delete,"-c":self.config_database, "test":self.test_func_with_two_args}
 
     def get_arguments(self):
         nrarguments = len(sys.argv)
-        if(nrarguments >= 1):
+        if(nrarguments >= 2):
             self._arguments = sys.argv[1:nrarguments]
             self._nr_arguments = len(self._arguments)
 
         print(self._arguments)
 
-
-'''
-#print('Number of arguments:', len(sys.argv), 'arguments.')
-#print('Argument List:', str(sys.argv))
-
-##För debug
-if(sys.argv[1].lower == "print"):
-    pass
-
-##För att lägga till ett projekt i databasen
-elif(sys.argv[1].lower == "add"):
-    pass
-
-elif(sys.argv[1].lower == "parse"):
-    Parser.parse(sys.argv[2])
-
-## För att ta bort ett projekt från databasen
-elif(sys.argv[1].lower == "delete"):
-    pass
-elif (sys.argv[1].lower == "-c"):
-    pass
-
-##efter denna ska en path till en fil med databaskonfig vara sen
-elif (sys.argv[1].lower== "-d"):
-    pass
-
-else:
-    pass
-'''
