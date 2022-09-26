3d15f1ee0a98dd4c611e2253a58ff56e32c6d833
Linus Thorsell <thorsell.linus@gmail.com>
2022-09-26 13:20:37 +0000

    basic table resizing

--- frontend/fmeca-frontend/src/components/VisTable.vue
+++ frontend/fmeca-frontend/src/components/VisTable.vue
@@ -1,16 +1,19 @@
 <script>
 
     const table_array = [];
-    
+    const columnWidths = [];
+
     function getTable() {
     
         const array_columns = 8;
         const array_rows = 4;
+        const default_column_width = 150;
     
         if (table_array.length === 0) {
             console.log("Array empty, creating example")
             for (var column = 0; column < array_columns; column++) {
                 table_array[column] = [];
+                columnWidths[column] = default_column_width;
                 for (var row = 0; row < array_rows; row++) {
                     table_array[column][row] = "Column: " + column + " Row: " + row;
                 }
@@ -50,7 +53,14 @@
     function addColumn() {
     
     }
- 
+
+    function getClass(column) {
+        return "vis-column-" + column
+    }
+    function getColumnWidth(column) {
+        return "width: " + columnWidths[column] + "px;"
+    }
+
     export default {
         data() {
             return {
@@ -58,7 +68,11 @@
                 getRow,
                 getColumn,
                 addRow,
-                addColumn,     
+                addColumn,
+
+                getClass,
+                getColumnWidth,
+                columnWidths: [],
             }
         }
     }
@@ -74,9 +88,34 @@
         <!--<div id="vis-header">
             Header info
         </div> -->
+        <!--<div class="vis-row">
+            <div v-for="column in getTable().length" 
+                class="vis-columnbox"
+                :style="getColumnWidth(column-1)"
+                :class="getClass(column-1)"
+            >
+                {{ column-1 }}
+            </div>
+        </div> -->
         <div v-for="row in getTable()[0].length" class="vis-row">
-            <div v-for="column in getTable().length" class="vis-columnbox">
-                row: {{ row-1 }} and col: {{ column-1 }}
+            <div class="vis-columnbox vis-resizable-row"> Resizable Row </div>
+            
+            <div v-if="row-1 === 0" v-for="column in getTable().length" 
+                class="vis-columnbox vis-resizable-column" 
+                :style="getColumnWidth(column-1)" 
+                :class="getClass(column-1)"
+            >
+                Resizable Column
+            </div>
+            
+            <div v-if="row-1 !== 0" v-for="column in getTable().length" 
+                class="vis-columnbox" 
+                :style="getColumnWidth(column-1)" 
+                :class="getClass(column-1)"
+            >
+                
+                <!-- <div v-if="row === 1" class="vis-columnbox vis-resizable-column"> Resizable Column </div> -->
+                <textarea class="vis-textarea">row: {{ row-1 }} and col: {{ column-1 }}</textarea>
             </div>
         </div>
     </div>
