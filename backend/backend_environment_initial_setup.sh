36aaadc9c223912e4cf1ff92cf716fe15f9f2511
Linus Thorsell <thorsell.linus@gmail.com>
2022-10-05 15:06:09 +0200

    little fix on setup script

--- backend/backend_environment_initial_setup.sh
+++ backend/backend_environment_initial_setup.sh
@@ -2,6 +2,7 @@
 sudo apt install python3 -y
 sudo apt install python3-venv -y
 sudo apt install mariadb-server -y
+rm -r ./fmeca-venv
 python3 -m venv ./fmeca-venv
 source ./fmeca-venv/bin/activate
 pip3 install -r requirements.txt
