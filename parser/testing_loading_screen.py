707a70730ffa268dc26536225f91fd1d1ca0190a
Oscar <oscar.sandell@live.se>
2022-12-04 18:51:27 +0100

    fixed loading screen

--- parser/testing_loading_screen.py
+++ parser/testing_loading_screen.py
@@ -8,7 +8,7 @@ sema = threading.Semaphore(0)
 
 stop = False
 
-def loading_screen():
+def loading_screen(string):
     #print("Press enter to stop the loading")    
     global steps
     global sema
@@ -17,12 +17,12 @@ def loading_screen():
     time.sleep(1)
     while True and stop == False:
         #print(steps[counter % many_steps], end="" ,flush=True)
-        sys.stdout.write("\r"+"Loading {0}".format(steps[counter % many_steps]))
+        sys.stdout.write("\r"+string+" {0}".format(steps[counter % many_steps]))
         sys.stdout.flush()        
         time.sleep(0.5)
         counter += 1
     sema.release()
-new_thread = threading.Thread(target=loading_screen)
+new_thread = threading.Thread(target=loading_screen,args=("Hej",))
 new_thread.start()
 input("Press any key to stop the thread\n")
 stop = True
